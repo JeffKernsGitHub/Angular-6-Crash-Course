4d81f4ef0bd919ea997e19ceef46d7e9a4835390
Jeff Kerns <jbkernsjob@gmail.com>
2018-06-24 14:15:26 -0400

    Fetching More Data from the API

--- ng6-proj/src/app/data.service.ts
+++ ng6-proj/src/app/data.service.ts
@@ -14,8 +14,15 @@ Med constructor and added getUSers() during HTTP Client
 */
  constructor(private http: HttpClient) { }
 
+ getUser(userId) {
+    return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
+  }
   getUsers()
   {
     return this.http.get('https://jsonplaceholder.typicode.com/users')
+  }
+  getPosts()
+  {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
   }
 }
