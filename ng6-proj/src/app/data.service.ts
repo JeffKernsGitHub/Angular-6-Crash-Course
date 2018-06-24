55e358578b76a18ed1ab86ca826750cfce847e5b
Jeff Kerns <jbkernsjob@gmail.com>
2018-06-24 12:45:45 -0400

    Angular 6 Services

---
+++ ng6-proj/src/app/data.service.ts
@@ -0,0 +1,13 @@
+/*
+purpose of our service file will be to communicate with an API via the Angular 6 HTTP Client.
+*/
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+}
