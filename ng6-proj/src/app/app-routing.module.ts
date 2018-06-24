a4033a9149da7a5f104a47a778f6abb4976b24d3
Jeff Kerns <jbkernsjob@gmail.com>
2018-06-24 12:40:53 -0400

    Angular 6 Routing Tutorial

--- ng6-proj/src/app/app-routing.module.ts
+++ ng6-proj/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+// add for Angular 6 Routing Tutorial
+const routes: Routes = [
+  {// specifies that the UsersComponent will be the default component that loads on the root path. 
+    path: '',
+    component: UsersComponent
+  },
+  {//route is for a user details section. We've specified a wildcard named id. We'll use this to fetch that value from the router in order to retrieve the correct user details.
+    path: 'details/:id',
+    component: DetailsComponent
+  },
+  { // same for posts 
+    path: 'posts',
+    component: PostsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
